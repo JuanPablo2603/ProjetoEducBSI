a7dfa85b2bd33edd41ceebf189b99739bad8d3e0
JuanPablo2603 <juanpabloaquino45@gmail.com>
2025-03-10 19:15:16 -0300

    Update Ex3Questao20.java

--- Ex(3)Questao20/src/ex/pkg3/questao20/Ex3Questao20.java
+++ Ex(3)Questao20/src/ex/pkg3/questao20/Ex3Questao20.java
@@ -83,7 +83,7 @@ public class Ex3Questao20 {
    
     public static void main(String[] args) {
         // TODO code application logic here
-        int votosFirmino = 0, votosEugenia = 0, votosNulos =0, votosBrancos =0, totalEleitores = 0;
+        nt votosFirmino = 0, votosEugenia = 0, votosNulos =0, votosBrancos =0, totalEleitores = 0;
          String vencedor = "";
         while(true){
             int voto = Integer.parseInt(JOptionPane.showInputDialog("Eleitor " + (totalEleitores +1) +
@@ -119,10 +119,14 @@ public class Ex3Questao20 {
                     "2 - Firmino"));
              if(voto == 1){
                  vencedor = "Eugenia" ;
+                 votosEugenia++;
              }
              else if(voto == 2){
                  vencedor = "Firmino";
+                 votosFirmino++;
              }
+             else JOptionPane.showMessageDialog(null, "Inválido");
+             totalEleitores++;
         } JOptionPane.showMessageDialog(null,
             "Resultados da Votação:\n" +
             "Eugenia: " + votosEugenia + " votos\n" +
